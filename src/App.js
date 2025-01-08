d18bf6b9f6178d9e743fe26bf66888a336183222
95hyun <kingmandoo95@gmail.com>
2025-01-08 18:21:12 +0900

    리액트 - 그룹 미니 프로젝트 - 랜덤 댕댕이 생성기

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RandomDog from "./components/randomDog";
+
+function App() {
+  return (
+    <div>
+      <RandomDog />
+    </div>
+  );
+}
+
+export default App;
